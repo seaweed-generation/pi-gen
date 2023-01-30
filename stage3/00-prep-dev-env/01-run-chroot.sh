b2ac21b9aa776fc33a8f59848fc7f9ca0f5f79d7
Dan Schaub <dan@danschaub.com>
2023-01-30 14:18:05 -0500

    cant enable i2c here, add some debugging

--- stage3/00-prep-dev-env/01-run-chroot.sh
+++ stage3/00-prep-dev-env/01-run-chroot.sh
@@ -3,6 +3,10 @@
 FIRST_USER_HOME="${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
 SSH_ID_FILE="${FIRST_USER_HOME}/.ssh/id_ed25519"
 
+echo "Setting up SSH"
+echo "Home directory -> $FIRST_USER_HOME"
+echo "SSH ID file -> $SSH_ID_FILE"
+
 mkdir -p $(dirname "$SSH_ID_FILE")
 
 echo "$ALGARITHMS_DEPLOY_KEY" > "$SSH_ID_FILE"
