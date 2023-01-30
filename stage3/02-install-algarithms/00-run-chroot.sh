a2d13d008ff5dedd99e0488d47b7b246126b11fd
Dan Schaub <dan@danschaub.com>
2023-01-30 15:37:30 -0500

    use identity file directly

--- stage3/02-install-algarithms/00-run-chroot.sh
+++ stage3/02-install-algarithms/00-run-chroot.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 runuser -l "$FIRST_USER_NAME" \
-  -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no\" pip install --user git+ssh://git@github.com/seaweed-generation/algarithms.git#egg=algarithms"
+  -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no -i /home/$FIRST_USER_NAME/.ssh/id_ed25519\" pip install --user git+ssh://git@github.com/seaweed-generation/algarithms.git#egg=algarithms"
 
 echo "*/2 * * * * $FIRST_USER_NAME .local/bin/alga wifi check" > /etc/cron.d/wifi-check
