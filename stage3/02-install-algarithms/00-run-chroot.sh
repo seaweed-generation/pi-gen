ffefa95790acce0684a3d3037d3ba41387db78f0
Dan Schaub <dan@danschaub.com>
2023-01-30 15:31:31 -0500

    add wifi check to cron

--- stage3/02-install-algarithms/00-run-chroot.sh
+++ stage3/02-install-algarithms/00-run-chroot.sh
@@ -1,1 +1,4 @@
-runuser -l "$FIRST_USER_NAME" -c "pip install --user git+ssh://git@github.com/seaweed-generation/algarithms.git#egg=algarithms"
+runuser -l "$FIRST_USER_NAME" \
+  -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no\" pip install --user git+ssh://git@github.com/seaweed-generation/algarithms.git#egg=algarithms"
+
+echo "*/2 * * * * $FIRST_USER_NAME .local/bin/alga wifi check" > /etc/cron.d/wifi-check
