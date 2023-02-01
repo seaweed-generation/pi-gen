06c31a13eff3666bb0e01b5232b02ef9f0498dce
Dan Schaub <dan@danschaub.com>
2023-02-01 15:19:10 -0500

    run wifi check every minute

--- stage3/02-install-algarithms/00-run-chroot.sh
+++ stage3/02-install-algarithms/00-run-chroot.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 runuser -l "$FIRST_USER_NAME" \
   -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no -i /home/$FIRST_USER_NAME/.ssh/id_ed25519\" pip install --user git+ssh://git@github.com/seaweed-generation/algarithms.git#egg=algarithms"
 
-echo "*/2 * * * * $FIRST_USER_NAME .local/bin/alga wifi check" > /etc/cron.d/wifi-check
+echo "* * * * * $FIRST_USER_NAME .local/bin/alga wifi check" > /etc/cron.d/wifi-check
