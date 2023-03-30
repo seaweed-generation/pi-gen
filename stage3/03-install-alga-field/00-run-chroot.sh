aa3b9b5f59ccf0c2f565b6bd30ff809b16469e5d
Dan Schaub <dan@danschaub.com>
2023-03-30 15:29:05 -0400

    try a different way of setting up db

--- stage3/03-install-alga-field/00-run-chroot.sh
+++ stage3/03-install-alga-field/00-run-chroot.sh
@@ -2,15 +2,20 @@
 
 set -euo pipefail
 
+USER_HOME="/home/$FIRST_USER_NAME"
+
 git_clone() {
   runuser -l "$FIRST_USER_NAME" \
-    -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no -i /home/$FIRST_USER_NAME/.ssh/id_ed25519\" git clone git@github.com:seaweed-generation/$1.git /home/$FIRST_USER_NAME/$1"
+    -c "GIT_SSH_COMMAND=\"ssh -o StrictHostKeyChecking=no -i $USER_HOME/.ssh/id_ed25519\" git clone git@github.com:seaweed-generation/$1.git $USER_HOME/$1"
 }
 
 git_clone "alga-field"
 
-runuser -l "$FIRST_USER_NAME" -c "cd /home/$FIRST_USER_NAME/alga-field && pip install --user -e ."
-runuser -l "$FIRST_USER_NAME" -c "cd /home/$FIRST_USER_NAME && .local/bin/alga db migrate"
+runuser -l "$FIRST_USER_NAME" -c "cd $USER_HOME/alga-field && pip install --user -e ."
+
+mkdir "$USER_HOME/.config"
+chown -R 1000:1000 "$USER_HOME/.config"
+runuser -l "$FIRST_USER_NAME" -c ".local/bin/alga db migrate"
 
 touch /etc/cron.d/alga-field
 
