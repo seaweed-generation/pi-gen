12d9025957a239c3895f0d891cd4cce1e97a8e7e
Dan Schaub <dan@danschaub.com>
2023-03-30 17:21:38 -0400

    fix another file name

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -8,7 +8,7 @@ source config.base
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 INFO_FILE="${BASE_DIR}/deploy/${IMG_FILENAME}-lite.info"
-ARCHIVE_FILE="${BASE_DIR}/deploy/${ARCHIVE_FILENAME}-lite.img.${DEPLOY_COMPRESSION}"
+ARCHIVE_FILE="${BASE_DIR}/deploy/${ARCHIVE_FILENAME}-lite.zip"
 
 aws s3 cp $INFO_FILE s3://algos-images
 aws s3 cp $ARCHIVE_FILE s3://algos-images
