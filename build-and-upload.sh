20401701ca385453c589266f4fec296f1093d1ae
Dan Schaub <dan@danschaub.com>
2023-03-30 17:20:17 -0400

    fix file names and add more cleanup

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -4,7 +4,15 @@ set -eu
 
 sudo ./build.sh
 
-aws s3 cp "${DEPLOY_DIR}/${IMG_FILENAME}${IMG_SUFFIX}.info" s3://algos-images
-aws s3 cp "${DEPLOY_DIR}/${IMG_FILENAME}${IMG_SUFFIX}.img.${DEPLOY_COMPRESSION}" s3://algos-images
+source config.base
+
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+INFO_FILE="${BASE_DIR}/deploy/${IMG_FILENAME}-lite.info"
+ARCHIVE_FILE="${BASE_DIR}/deploy/${ARCHIVE_FILENAME}-lite.img.${DEPLOY_COMPRESSION}"
+
+aws s3 cp $INFO_FILE s3://algos-images
+aws s3 cp $ARCHIVE_FILE s3://algos-images
 
 sudo rm -rf work
+sudo rm $INFO_FILE
+sudo rm $ARCHIVE_FILE
