ad3876c20a36296eaa0b830f18f31f32433278f4
Dan Schaub <dan@seaweedgeneration.com>
2024-02-06 12:47:18 -0500

    add mender client

--- stage3/00-install-packages/00-run-chroot.sh
+++ stage3/00-install-packages/00-run-chroot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+echo "Setting up tailscale apt repo..."
+
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
--- stage3/01-first-boot-provision/00-run.sh
+++ stage3/01-first-boot-provision/00-run.sh
@@ -4,10 +4,11 @@
 if [[ -z "$TAILSCALE_AUTH_KEY" ]]; then
   echo "TAILSCALE_KEY_KEY is not set, will need to manually run tailscale up"
 else
+  echo "Installing tailscale auth key..."
   echo "$TAILSCALE_AUTH_KEY" > "${ROOTFS_DIR}/etc/tailscale-auth"
 fi
 
-
+# install the firstboot service which runs firstboot.sh and renames it when it's done
 install -m 644 files/firstboot.service "${ROOTFS_DIR}/lib/systemd/system/"
 
 on_chroot <<EOF
