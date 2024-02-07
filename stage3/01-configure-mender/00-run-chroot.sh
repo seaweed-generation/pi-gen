2678f21f9fb2d36c38d7a7e5423e8e36590a3f8f
Dan Schaub <dan@seaweedgeneration.com>
2024-02-07 15:23:56 -0500

    added mender config

---
+++ stage3/01-configure-mender/00-run-chroot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+if [[ -z "$MENDER_TENANT_TOKEN" || -z "$MENDER_DEVICE_TYPE" ]]; then
+  echo "MENDER_TENANT_TOKEN or MENDER_DEVICE_TYPE is not set, will need to manually run mender setup"
+else
+  echo "Setting up mender config..."
+
+  mender setup \
+    --device-type "$MENDER_DEVICE_TYPE" \
+    --hosted-mender \
+    --tenant-token "$MENDER_TENANT_TOKEN" \
+    --demo-polling
+fi
