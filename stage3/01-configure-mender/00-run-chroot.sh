088d36339e17dea3a46fba9f2cb6c14f5cf2651b
Dan Schaub <dan@seaweedgeneration.com>
2024-02-08 13:32:30 -0500

    explain whats up with the --data flag

--- stage3/01-configure-mender/00-run-chroot.sh
+++ stage3/01-configure-mender/00-run-chroot.sh
@@ -6,6 +6,10 @@ else
   echo "Setting up mender config..."
 
   mender setup \
+    # the community-maintained deb package symlinks /var/lib/mender to /data/mender,
+    # but the setup command tries to also create /var/lib/mender, so it blows up.
+    # not sure if that'll all be fixed in the new setup stuff, which should be released
+    # for bookworm imminently, but this works for now.
     --data /data/mender \
     --device-type "$MENDER_DEVICE_TYPE" \
     --hosted-mender \
