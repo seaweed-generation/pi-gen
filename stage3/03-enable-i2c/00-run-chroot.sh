1849740b3994bddd8f0939206df1e7ab6a17fa7b
Dan Schaub <dan@seaweedgeneration.com>
2024-03-29 10:57:20 -0400

    add i2c-dev to enabled kernel modules

--- stage3/03-enable-i2c/00-run-chroot.sh
+++ stage3/03-enable-i2c/00-run-chroot.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
 
 sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/firmware/config.txt
+
+sed /etc/modules -i -e "s/^#[[:space:]]*\(i2c[-_]dev\)/\1/"
+if ! grep -q "^i2c[-_]dev" /etc/modules; then
+  echo "i2c-dev\n" >> /etc/modules
+fi
