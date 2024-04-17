cfb1c4ff59c4c81414ccbd0e41c3330119492a8a
Dan Schaub <dan@seaweedgeneration.com>
2024-04-17 12:21:00 -0400

    use printf instead of echo

--- stage3/03-enable-i2c/00-run-chroot.sh
+++ stage3/03-enable-i2c/00-run-chroot.sh
@@ -4,5 +4,5 @@ sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/firmware/config.txt
 
 sed /etc/modules -i -e "s/^#[[:space:]]*\(i2c[-_]dev\)/\1/"
 if ! grep -q "^i2c[-_]dev" /etc/modules; then
-  echo "i2c-dev\n" >> /etc/modules
+  printf "i2c-dev\n" >> /etc/modules
 fi
