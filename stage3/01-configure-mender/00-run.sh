4cef6c33d0fe4edc95691189d2ae319617a1a17a
Dan Schaub <dan@seaweedgeneration.com>
2024-09-04 11:19:48 -0400

    remove mender

--- stage3/01-configure-mender/00-run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-install -m 755 files/mender-device-identity "${ROOTFS_DIR}/usr/share/mender/identity/mender-device-identity"
-install -m 644 files/artifact_info "${ROOTFS_DIR}/etc/mender/artifact_info"
